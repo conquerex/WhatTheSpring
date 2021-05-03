b5d8735b9ac75085ba0b6d50eb1a39dedeec361c
Jongkook <barley.son@Jongkooks-MacBook-Pro.local>
2021-05-03 14:14:57 +0900

    init

---
+++ src/main/java/what/the/spring/WhatTheSpring/WhatTheSpringApplication.java
@@ -0,0 +1,13 @@
+package what.the.spring.WhatTheSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatTheSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatTheSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/what/the/spring/WhatTheSpring/WhatTheSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package what.the.spring.WhatTheSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatTheSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
